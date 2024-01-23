9383be7429d5b6715b2b97a74b87af6b6992efa4
FasolowySpioch <fasolowyspioch@gmail.com>
2024-01-23 18:47:31 +0100

    commit

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('add-human',[PeopleController::class, 'adding']);
-Route::put('edit-human',[PeopleController::class, 'edit']);
-Route::delete('remove-record',[PeopleController::class, 'removing']);
-Route::get('get-human/{id}', [PeopleController::class, 'getOne']);
-Route::get('get-humans', [PeopleController::class, 'getAll']);
+Route::post('ziemska/310848/add-human',[PeopleController::class, 'adding']);
+Route::put('ziemska/310848/edit-human',[PeopleController::class, 'edit']);
+Route::delete('ziemska/310848/remove-record',[PeopleController::class, 'removing']);
+Route::get('ziemska/310848/get-human/{id}', [PeopleController::class, 'getOne']);
+Route::get('ziemska/310848/get-humans', [PeopleController::class, 'getAll']);
 
 //Illuminate\Database\QueryException: SQLSTATE[HY000] [2002] Nie można nawiązać połączenia, ponieważ komputer docelowy aktywnie go odmawia (Connection: mysql, SQL: select * from `people`) in file C:\Users\fasol\Desktop\studia_repozytorium\laravel-API\vendor\laravel\framework\src\Illuminate\Database\Connection.php on line 822
